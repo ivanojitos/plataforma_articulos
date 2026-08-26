66325414c8eca126975c0611b8c2187df07f1bb8
Ivan Alvarez <ivan.alvarez@servimsa.com.mx>
2026-08-26 09:49:06 -0600

    feat: agregue rutas

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ArticuloController;
+use App\Http\Controllers\Admin\ArticuloController as AdminArticuloController;
+use App\Http\Controllers\Admin\DashboardController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -11,10 +15,21 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/', function () {
+    return redirect()->route('articles.index');
+});
+
+Route::get('/articulos', [ArticuloController::class, 'index'])->name('articles.index');
+Route::get('/articulos/{article}', [ArticuloController::class, 'show'])->name('articles.show');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::get('/', DashboardController::class)->name('dashboard');
+    Route::resource('articulos', AdminArticuloController::class)->except(['show']);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
